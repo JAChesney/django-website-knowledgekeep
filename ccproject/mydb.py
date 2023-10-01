fc9b383ac1cf60aa5361deed8c4e545de2f352a3
Jason Chesney <jasonchesney29@gmail.com>
2023-10-01 22:59:15 +0530

    Second Commit

---
+++ ccproject/mydb.py
@@ -0,0 +1,15 @@
+from mysql import connector
+
+dataBase = connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = "*222bda03jason*",
+)
+
+# prepare a cursor object
+cursorObject = dataBase.cursor()
+
+# create a database
+cursorObject.execute('create database if not exists knowledgekeepdb')
+
+print('All done!!')
