76c2a4c380cb0ea2ca145a460dc30fd01681dd42
Jason Chesney <jasonchesney29@gmail.com>
2023-10-03 01:32:57 +0530

    Creation of login and signup pages done

--- ccproject/knowledgekeep/views.py
+++ ccproject/knowledgekeep/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'mainfiles/home.html', {}) 
+
+def userlogin(request):
+    return render(request, 'mainfiles/login.html', {})
+
+def signup(request):
+    return render(request, 'mainfiles/signup.html', {})
