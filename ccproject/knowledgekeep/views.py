78caa32bae50a6b0ebcb276d1185003441edfafb
Jason Chesney <jasonchesney29@gmail.com>
2023-10-04 00:01:41 +0530

    Registration working and assets added

--- ccproject/knowledgekeep/views.py
+++ ccproject/knowledgekeep/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .models import UserProfile
 
 # Create your views here.
 def home(request):
@@ -8,4 +10,36 @@ def userlogin(request):
     return render(request, 'mainfiles/login.html', {})
 
 def signup(request):
+    print(request.method)
+    # Once using POST add the name to the header and check
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        print(username, email, password)
+
+        # Check whether user already exist
+
+        # Adding user to database
+        user = UserProfile(full_name=username, email=email)
+        user.set_password(password)
+        user.save()
+
+        user = UserProfile.objects.get(email=email)
+
+        # Authenticate user
+        user = authenticate(email=email, password=password)
+
+        print(user)
+
+        # Log user in
+        if not user:
+            print('Error logging in!')
+
+        
+        login(request, user)
+
+        # Redirecting to home
+        return redirect('/')
+
     return render(request, 'mainfiles/signup.html', {})
